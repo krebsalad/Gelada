b707c138ff289c37053a9703484ddd892db5f537
johan mgina <johanmgina7@gmail.com>
2020-10-10 15:11:14 +0200

    added another webscraping script

--- README.md
+++ README.md
@@ -34,16 +34,36 @@ node scripts/csv_to_ttl.js in:=data/VideoGames.csv out:=data/VideoGames.ttl  cha
 cat data/VideoGames.ttl
 ```
 
-### running html_to_csv -> csv_to_tll
+### running html_to_csv_genres and the to csv_to_tll
 1. have [nodejs](https://nodejs.org/en/download/) installed:  
 2. run a terminal, clone this repository and go into the project work space
 ```
 git clone https://github.com/krebsalad/Gelada
 cd Gelada
 ```
-3. run the script html_to_csv.js, this pull data about consoles from https://www.videogameconsolelibrary.com/main-list-name.htm
+3. run the script html_to_csv_genres.js, this will scrape data from https://vsrecommendedgames.fandom.com/wiki/A_List_and_Guide_to_Game_Genres and save it as csv in data/VideoGamesGenres.csv
 ```
-node scripts/html_to_csv.js
+node scripts/html_to_csv_genres.js
+```
+4. run the script csv_to_tll to convert the resulting file.
+```
+node scripts/csv_to_ttl.js in:=data/VideoGameGenres.csv out:=data/VideoGameGenres.ttl  char_newelement:=";;" subject:="Genre"
+```
+5. see the output of the script in the directory data/VideoGameGenres.ttl
+```
+cat data/VideoGameConsoles.ttl
+```
+
+### running html_to_csv_consoles and the to csv_to_tll
+1. have [nodejs](https://nodejs.org/en/download/) installed:  
+2. run a terminal, clone this repository and go into the project work space
+```
+git clone https://github.com/krebsalad/Gelada
+cd Gelada
+```
+3. run the script html_to_csv_genres.js, this will scrape data from https://www.videogameconsolelibrary.com/main-list-name.htm and save it as csv in data/VideoGamesGenres.csv 
+```
+node scripts/html_to_csv_consoles.js
 ```
 4. run the script csv_to_tll to convert the resulting file.
 ```
---
+++ scripts/html_to_csv_genres.js
@@ -0,0 +1,121 @@
+/*
+*       Made 10-10-2020 with vscode and nodejs v12.18.4
+*       This script can be used to scrape data from vsrecommendedgames.fandom.com/wiki/A_List_and_Guide_to_Game_Genres
+*       
+*/
+
+function getXmlValue(in_str)
+{
+  var out_str = in_str.match(/>([^>]*)</)[0];
+  out_str = out_str.replace(/>|</g, '');
+  out_str = out_str.replace(/\r?\n/, '');
+  return out_str;
+}
+
+function getXmlTagBetweenMultipleLines(match_tag, match_start, in_str)
+{
+  var matching_txts = in_str.split('<' + match_tag + match_start);
+  matching_txts.shift();
+  var out_strs = []
+  for (t in matching_txts)
+  {
+    var lines = matching_txts[t].split(/\r?\n/);
+    var inwards = 1;
+    var end_line = 0;
+    var out_str = '';
+    for (l in lines)
+    {
+      out_str += lines[l] + "\n";
+
+      var matches = lines[l].match(new RegExp('<' + match_tag, 'g'));
+      if(matches)
+      if (matches.length)
+      {
+        inwards += matches.length;
+      }
+      var matches = []
+      var matches = lines[l].match(new RegExp('/' + match_tag + '>', 'g'));
+      if(matches)
+      if(matches.length)
+      {
+        inwards -= matches.length;
+        if(inwards == 0)
+        {
+          break;
+        }
+      }
+    }
+
+    out_strs.push(out_str);
+  }
+  
+  return out_strs;
+}
+
+// get video game categories
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+var http = require('https');
+var options = {
+  host: 'vsrecommendedgames.fandom.com',
+  path: '/wiki/A_List_and_Guide_to_Game_Genres',
+  headers: headers
+};
+callback = function(response) {
+  var str = '';
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('end', function () {
+    var main_container = getXmlTagBetweenMultipleLines('article', ' id="WikiaMainContent"', str);
+    var tables = getXmlTagBetweenMultipleLines('table', ' width', main_container[0]);
+    var table_body = getXmlTagBetweenMultipleLines('tbody', '', tables[0]);
+    var table_resources = getXmlTagBetweenMultipleLines('tr', '', table_body[0]);
+
+    var structured_data_headers = [];
+    var structured_data = [];
+
+    var header_tags = getXmlTagBetweenMultipleLines('th', '', table_resources[0]);
+    for (t in header_tags)
+    {
+      structured_data_headers.push(getXmlValue(header_tags[t]));
+    }
+    console.log("headers:" + structured_data_headers);
+    table_resources.shift();
+
+    for (r in table_resources)
+    {
+      var genrename_tag = getXmlTagBetweenMultipleLines('th', '', table_resources[r]);
+      var description_tags = getXmlTagBetweenMultipleLines('td', '', table_resources[r]);
+      var elem = [];
+      elem.push(getXmlValue(genrename_tag[0]));
+      elem.push(getXmlValue(description_tags[0]));
+      elem.push(getXmlValue(description_tags[1]));
+      structured_data.push(elem);
+    }
+    console.log("found " + structured_data.length + " instances" );
+
+    // convert to csv
+    var out_data = "";
+    for (d in structured_data_headers)
+    {
+      if(d!=0) out_data+= ";;";
+      out_data += String(structured_data_headers[d])
+    }
+    out_data += "\n";
+    for (d in structured_data)
+    {
+      for (d2 in structured_data[d])
+      {
+        if(d2!=0) out_data+= ";;";
+        out_data += String(structured_data[d][d2]);
+      }
+      out_data += "\n";
+    }
+    var fs = require('fs');
+    fs.writeFileSync("data/VideoGameGenres.csv", out_data);
+    console.log("wrote instances as csv format to " + "data/VideoGameGenres.csv" + " with comma as ;; and newline as \\n ");
+
+  });
+}
+http.request(options, callback).end();
